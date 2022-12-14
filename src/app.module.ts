3169455ce1e91333bbfec43818755c84e0f5ce54
borgainkinq <borgainkinq@gmail.com>
2022-12-14 23:18:56 +0900

    [fix] TypeOrm 모듈 dotenv 환경변수 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'root',
-    //   database: 'test',
-    //   entities: [],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_SCHEMA,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
