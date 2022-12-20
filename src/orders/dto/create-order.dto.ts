3d5304d97d39c574a2a79b318f4dbfdd2116315f
borgainkinq <borgainkinq@gmail.com>
2022-12-20 18:51:00 +0900

    [fix] Add Order Create Validation

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,15 +1,32 @@
-import { IsArray, IsDateString, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsDateString,
+  IsString,
+  IsOptional,
+  IsObject,
+} from 'class-validator';
 
 export class CreateOrderDto {
   @IsString()
   readonly status: string;
 
   @IsString()
-  readonly notice: string;
+  readonly pickUpMethod: string;
 
   @IsDateString()
   readonly pickUpDateTime: Date;
 
+  @IsObject()
+  readonly address: Object;
+
+  @IsDateString()
+  @IsOptional()
+  readonly wishLaundryDateTime: Date;
+
+  @IsString()
+  @IsOptional()
+  readonly notice: string;
+
   @IsArray()
-  readonly productIds: string[];
+  readonly products: string[];
 }
