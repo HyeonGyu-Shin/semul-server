fbb918eec6d29ea73ad6fc69ee00c4240c72606f
borgainkinq <borgainkinq@gmail.com>
2022-12-20 18:50:27 +0900

    [feat] OrderProduct 저장 기능 - Order 생성 시 기록 저장 가능 - Repository, Service 구현 - 모듈 추가

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsRepository } from '../products/repository/products.repository';
 import { Order } from '../entities/order.entity';
 import { OrdersController } from './controller/orders.controller';
 import { OrdersRepository } from './repository/orders.repository';
 import { OrdersService } from './service/orders.service';
+import { OrderProductsRepository } from '../order_products/repository/order_products.repository';
+import { OrderProductsService } from '../order_products/service/order_products.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order])],
   controllers: [OrdersController],
-  providers: [OrdersRepository, OrdersService],
+  providers: [
+    OrdersRepository,
+    OrdersService,
+    ProductsRepository,
+    OrderProductsRepository,
+    OrderProductsService,
+  ],
 })
 export class OrdersModule {}
--- src/orders/service/orders.service.ts
+++ src/orders/service/orders.service.ts
@@ -2,18 +2,31 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Order } from '../../entities/order.entity';
 import { OrdersRepository } from '../repository/orders.repository';
 import { CreateOrderDto } from './../dto/create-order.dto';
+import { OrderProductsService } from './../../order_products/service/order_products.service';
 
 @Injectable()
 export class OrdersService {
-  constructor(private ordersRepository: OrdersRepository) {}
+  constructor(
+    private ordersRepository: OrdersRepository,
+    private orderProductsService: OrderProductsService,
+  ) {}
 
-  async create({ status, notice, pickUpDateTime, productIds }: CreateOrderDto) {
-    await this.ordersRepository.save({
+  async create({
+    status,
+    pickUpMethod,
+    pickUpDateTime,
+    address,
+    products,
+  }: CreateOrderDto) {
+    const order = await this.ordersRepository.save({
       status,
-      notice,
+      pickUpMethod,
       pickUpDateTime,
-      products: productIds,
+      address,
+      products,
     });
+
+    await this.orderProductsService.create(order, order.products);
   }
 
   findAll(): Promise<Order[]> {
@@ -30,7 +43,7 @@ export class OrdersService {
     return order;
   }
 
-  async findByStatus(status: string): Promise<Order[]> {
-    return await this.ordersRepository.findByStatus(status);
+  findByStatus(status: string): Promise<Order[]> {
+    return this.ordersRepository.findByStatus(status);
   }
 }
