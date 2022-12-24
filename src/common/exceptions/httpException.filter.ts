02a9df2cb1d3414f0a60e6ef197afac1917e87fb
hyeongyu shin <rkrkdldkd@gmail.com>
2022-12-25 02:12:17 +0900

    [feat] Logger & Error Logger 추가

--- src/common/exceptions/httpException.filter.ts
+++ src/common/exceptions/httpException.filter.ts
@@ -3,16 +3,22 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  Logger,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
     const message = exception.getResponse();
+
+    Logger.error(
+      `${request.method} ${request.originalUrl} ${JSON.stringify(message)}`,
+    );
 
     response.status(status).json({
       message,
