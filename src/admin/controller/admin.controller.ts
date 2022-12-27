d85a48c03b5488057e15a290ac0041aac05005f6
hyeongyu shin <rkrkdldkd@gmail.com>
2022-12-27 11:44:43 +0900

    [feat] 어드민 삭제 기능 추가

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from 'src/common/enums/role.enum';
+import { LaundryDto } from 'src/laundries/dto/laundryDto';
 import { AdminService } from '../service/admin.service';
 
 @Controller('admin')
@@ -10,6 +19,7 @@ import { AdminService } from '../service/admin.service';
 @Roles(Role.Admin)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
   @Get('users')
   async getAllUsers() {
     return this.adminService.findAllUsers();
@@ -20,13 +30,36 @@ export class AdminController {
     return this.adminService.findAllPartners();
   }
 
+  @Put('partners/:id')
+  async changePartnerInfo(
+    @Param('id') laundryId: string,
+    @Body() laundryDto: LaundryDto,
+  ) {
+    return this.adminService.updatePartnerInfo(laundryId, laundryDto);
+  }
+
+  @Delete('users')
+  async deleteUser(@Body('userId') userId: string) {
+    return this.adminService.deleteUser(userId);
+  }
+
   @Get('orders')
   async getAllOrders() {
     return 'get All orders';
   }
 
+  @Delete('orders')
+  async deleteOrder(@Body('orderId') orderId: string) {
+    return this.adminService.deleteOrder(orderId);
+  }
+
   @Get('products')
   async getAllProducts() {
     return this.adminService.findAllProducts();
+  }
+
+  @Delete('products')
+  async deleteProduct(@Body('productId') productId: string) {
+    return this.adminService.deleteProduct(productId);
   }
 }
--- src/admin/service/admin.service.ts
+++ src/admin/service/admin.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { AddressRepository } from 'src/address/repository/address.repository';
+import { LaundryDto } from 'src/laundries/dto/laundryDto';
 import { LaundriesRepository } from 'src/laundries/repository/laundries.repository';
+import { OrdersRepository } from 'src/orders/repository/orders.repository';
 import { ProductsRepository } from 'src/products/repository/products.repository';
 import { UsersRepository } from 'src/users/repository/users.repository';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AdminService {
@@ -9,6 +13,9 @@ export class AdminService {
     private readonly usersRepository: UsersRepository,
     private readonly laundriesRepository: LaundriesRepository,
     private readonly productsRepository: ProductsRepository,
+    private readonly addressRepository: AddressRepository,
+    private readonly ordersRepository: OrdersRepository,
+    private dataSource: DataSource,
   ) {}
 
   async findAllUsers() {
@@ -19,7 +26,49 @@ export class AdminService {
     return await this.laundriesRepository.findAllLaundries();
   }
 
+  async updatePartnerInfo(laundryId: string, laundryDto: LaundryDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const { manager } = queryRunner;
+
+    try {
+      const foundLaundry = await this.laundriesRepository.findOne(laundryId);
+
+      const newAddress = laundryDto.address.toAddressEntity();
+      newAddress.id = foundLaundry.address.id;
+
+      const newLaundry = laundryDto.toLaundryEntity();
+      newLaundry.id = foundLaundry.id;
+
+      await this.addressRepository.updateOneInTransaction(manager, newAddress);
+
+      await this.laundriesRepository.updateOneByEm(manager, newLaundry);
+
+      queryRunner.commitTransaction();
+      return '성공!';
+    } catch (err) {
+      queryRunner.rollbackTransaction();
+      return err;
+    }
+  }
+
   async findAllProducts() {
     return await this.productsRepository.findAllProducts();
+  }
+
+  async deleteUser(userId: string) {
+    await this.usersRepository.deleteOne(userId);
+    return '유저가 삭제되었습니다!';
+  }
+
+  async deleteOrder(orderId: string) {
+    await this.ordersRepository.softDelete(orderId);
+    return '주문이 삭제되었습니다.';
+  }
+
+  async deleteProduct(productId: string) {
+    await this.productsRepository.delete(productId);
+    return '상품이 삭제되었습니다.';
   }
 }
